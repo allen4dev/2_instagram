ff88b856b699c273670cec6edc955595d329f9fa
allen4dev <alanaliagadev@gmail.com>
2017-09-19 22:59:20 -0400

    [Add] v1

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -5,3 +5,21 @@ declare var module: {
     accept(path: string, callback: () => void): void,
   },
 };
+
+// export type State = {
+//   searchTerm: string,
+//   apiData: {
+//     [imdbID: string]: Show,
+//   },
+// };
+
+declare type ActionType = 'SET_SEARCH_TERM' | 'ADD_API_DATA';
+
+declare type ActionT<A: ActionType, P> = {|
+  type: A,
+  payload: P,
+|};
+
+export type Action =
+  | ActionT<'SET_SEARCH_TERM', string>
+  | ActionT<'ADD_API_DATA', Show>;
